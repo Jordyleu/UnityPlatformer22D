5da77f45e8f912bbcdaeac6fc6f0003e5c67f2aa
Gabriel <gabriel.martin20051230@gmail.com>
2024-05-03 12:56:28 +0200

    délimitation de caméra
    la caméra ne peux pas dépasser l'écran du jeu

--- SuperSquareLand-clean-version/Assets/SSL/Runtime/Scripts/Camera/CameraManager.cs
+++ SuperSquareLand-clean-version/Assets/SSL/Runtime/Scripts/Camera/CameraManager.cs
@@ -45,6 +45,7 @@ public class CameraManager : MonoBehaviour
     {
 
         Vector3 nextPosition = _FindCameraNextPosition();
+        nextPosition = _ClampPositionIntoBounds(nextPosition);
         nextPosition = _ApplyDamping(nextPosition);
         if (IsPlayingProfileTransition())
         {
@@ -65,7 +66,7 @@ public class CameraManager : MonoBehaviour
         _currentCameraProfile = _defaultCameraProfile;
         _SetCameraPositon(_currentCameraProfile.Position);
         _SetCameraSize(_currentCameraProfile.CameraSize);
-        _SetCameraDampedPosition(_FindCameraNextPosition());
+        _SetCameraDampedPosition(_ClampPositionIntoBounds(_FindCameraNextPosition()));
     }
 
     public void EnterProfile(CameraProfile cameraProfile, CameraProfileTransition transition = null)
@@ -166,5 +167,34 @@ public class CameraManager : MonoBehaviour
     {
         _dampedPosition.x = position.x;
         _dampedPosition.y = position.y;
+    }
+
+    private Vector3 _ClampPositionIntoBounds(Vector3 position)
+    {
+        if (!_currentCameraProfile.HasBounds) return position;
+
+        Rect boundsRect = _currentCameraProfile.BoundsRect;
+        Vector3 worldBottomLeft = _camera.ScreenToWorldPoint(new Vector3(0f, 0f));
+        Vector3 worldTopRight = _camera.ScreenToWorldPoint(new Vector3(_camera.pixelWidth, _camera.pixelHeight));
+        Vector2 worldScreenSize = new Vector2(worldTopRight.x - worldBottomLeft.x, worldTopRight.y - worldBottomLeft.y);
+        Vector2 worldHalfScreenSize = worldScreenSize / 2f;
+
+        if (position.x > boundsRect.xMax - worldHalfScreenSize.x)
+        {
+            position.x = boundsRect.xMax - worldHalfScreenSize.x;
+        }
+        if (position.x < boundsRect.xMin + worldHalfScreenSize.x)
+        {
+            position.x = boundsRect.xMin + worldHalfScreenSize.x;
+        }
+        if (position.y > boundsRect.yMax - worldHalfScreenSize.y)
+        {
+            position.y = boundsRect.yMax - worldHalfScreenSize.y;
+        }
+        if (position.y < boundsRect.yMin + worldHalfScreenSize.y)
+        {
+            position.y = boundsRect.yMin + worldHalfScreenSize.y;
+        }
+        return position;
     }
 }
